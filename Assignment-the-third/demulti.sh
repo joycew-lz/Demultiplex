fcc88fc7d284edc8c0985a458538c60e1a2e3102
Joyce Wang <joycew@login4.talapas.uoregon.edu>
2025-09-03 09:24:34 -0700

    uploading demultiplexing sh script

--- Assignment-the-third/demulti.sh
+++ Assignment-the-third/demulti.sh
@@ -11,11 +11,6 @@
 
 # didn't create an environment or activate it here, but I should!
 
-#/usr/bin/time -v ./demulti.py -rp1 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz \
-#-rp2 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz \
-#-i1 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz \
-#-i2 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz
-
 /usr/bin/time -v ./demulti.py -rp1 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz \
 -rp2 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz \
 -i1 /projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz \
